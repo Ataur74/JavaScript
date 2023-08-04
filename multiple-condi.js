606e388b9ce198ce06cb6e25fa0dc4b0ed6bbde9
Ataur71 <ataur7084@gmail.com>
2023-08-04 15:14:20 +0600

    multiple conditionals practise

---
+++ multiple-condi.js
@@ -0,0 +1,13 @@
+var isGraduated = false;
+if(isGraduated==true){
+      console.log('I will give you a job.')
+}
+else{ console.log('You canot stay in Dhaka; You had better leave this city soon.')}
+
+
+var myMoney = 98500;
+
+if('myMoney'>=50000){
+      console.log('I will give you a job.')
+}
+else{ console.log('You canot stay in Dhaka; You had better leave this city soon.')}
