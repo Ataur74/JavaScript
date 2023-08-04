60d198341d84f525fce05d6a625fcb1017ff9d1f
Ataur71 <ataur7084@gmail.com>
2023-08-04 14:51:07 +0600

    practised if and if-else

---
+++ fahim.js
@@ -0,0 +1,16 @@
+var iphonePrice = 97500;
+var myBudget = 9500;
+if(iphonePrice<=myBudget){
+    console.log('I will buy an iphone.')
+} 
+else{
+    console.log('I will wait for saving more money.')
+}
+
+myBudget = 98500;
+if(iphonePrice<=myBudget){
+    console.log('I must buy an iphone.')
+} 
+else{
+    console.log('I will wait.')
+}
