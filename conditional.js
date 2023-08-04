f6c589e82526e464f35456ad9a4f745b177d238d
Ataur71 <ataur7084@gmail.com>
2023-08-04 12:27:11 +0600

    simple java experience

---
+++ conditional.js
@@ -0,0 +1,15 @@
+console.log(5);
+console.log(5+7);
+var firstName = 'Md Ataur';
+var lastName = 'Rahman';
+console.log(firstName + ' ' + lastName);
+var x=3;
+var y=7;
+console.log(x+y);
+
+var x = 15;
+var y = 11;
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
