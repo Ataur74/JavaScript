1cd75f49d5bceb4cd4a5fba4a1180a7f2666cb88
Ataur71 <ataur7084@gmail.com>
2023-08-05 23:24:55 +0600

    indexof

---
+++ exist.js
@@ -0,0 +1,2 @@
+const myLyrics='I love you more than I can say. O dear "Sweet Heart!" I see the golden moments you bring. I feel every bend you take to see me.'
+console.log(myLyrics.indexOf('moments'));
