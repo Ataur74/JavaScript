246f8192c4ef5ed321f97f360aa6d5c193892006
Ataur71 <ataur7084@gmail.com>
2023-08-06 00:06:37 +0600

    swap

---
+++ mathPow.js
@@ -0,0 +1,11 @@
+//const result= Math.pow(10,3);
+//console.log(result);//
+
+//let x=10; y=15;
+//[y=10,x=15];
+//console.log(x,y);
+
+var firstName='Rahman';
+var lastName='Ataur';
+[firstName='Ataur', lastName='Rahman'];
+console.log(firstName,lastName);
