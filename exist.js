62f504c45551a33bf537db3c3f5292dd02bae336
Ataur71 <ataur7084@gmail.com>
2023-08-05 23:46:16 +0600

    word out of string is 'not found'

--- exist.js
+++ exist.js
@@ -1,2 +1,6 @@
 const myLyrics='I love you more than I can say. O dear "Sweet Heart!" I see the golden moments you bring. I feel every bend you take to see me.'
-console.log(myLyrics.indexOf('moments'));
+console.log(myLyrics.indexOf('boring'));
+if(myLyrics.indexOf('boring')==-1){
+    console.log('Not found.');
+
+}
