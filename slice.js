e73f4b8cc056bb622930b3c487dfccae6cbbf761
Ataur71 <ataur7084@gmail.com>
2023-08-05 23:15:19 +0600

    lyrics split

---
+++ slice.js
@@ -0,0 +1,6 @@
+const lyrics='I love you more than I can say. O dear "Sweet Heart!" I see the golden moments you bring. I feel every bend you take to see me.'
+console.log(lyrics);
+const parts=lyrics.split(' ');
+console.log(parts);
+const sentences=lyrics.split(' . ');
+console.log(sentences);
