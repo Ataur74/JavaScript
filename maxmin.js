ca54fa2c3c173f843b7158df5400b8b5ecb5cb37
Ataur71 <ataur7084@gmail.com>
2023-08-06 20:07:50 +0600

    duplicate out

---
+++ maxmin.js
@@ -0,0 +1,21 @@
+const numbers = [45, 192, 25, 65, 55, 48, 77, 91, 312, 125];
+function maxInArray(numbers) {
+
+    let largest = numbers[0];
+    for (i = 0; i < numbers.length; i++) {
+        const index = i;
+        const element = numbers[index];
+        if (element > largest) {
+            largest = element;
+        }
+        
+        const largest = maxInArray
+        console.log(largest);
+    }
+     else {('not found');
+}
+    }
+    console.log(largest);
+
+
+
