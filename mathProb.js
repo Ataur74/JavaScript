4e7d35c435867fe6682923f03b99140579d85ea1
Ataur71 <ataur7084@gmail.com>
2023-08-08 08:21:35 +0600

    while loop practice

---
+++ mathProb.js
@@ -0,0 +1,20 @@
+//for (let i = 1; i <= 50; i++) {
+  //  console.log(i);
+    //if (i % 3 === 0) {
+      //  console.log('foo');
+
+//else (i % 5 === 0); {
+  //          console.log('bar');
+       // }
+
+    //}
+    //else {
+      //  console.log(i)
+    //}
+//}
+
+var num = 0;
+while(num<1){
+console.log('Hello!');
+num+=1;
+}
